dd88a2c22707521346b73830c732e8f5d8f606b8
taciga30 <taciga30@gmail.com>
2023-09-04 11:03:33 -0500

    Faltan 3 funciones

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,22 +1,41 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const palabras = text.split(" ");
+    const numpalabras = palabras.filter((palabras) => palabras !== "");
+    return numpalabras.length;
   },
+
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length;
   },
+
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    const textWithoutPunctuation = text.replace(/[^\w\s]/g, " ");
+    const textWithoutSpaces = textWithoutPunctuation.replace(/\s/g, "");
+    return textWithoutSpaces.length;
   },
-  getAverageWordLength: (text) => {    
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  },
+
+  //getAverageWordLength: (text) => {
+  //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+  //¿Cómo se calcula la longitud media de palabras en una cadena de texto?
+  //Para calcular la longitud media de palabras en una cadena de texto, primero hay que dividir el texto en palabras individuales. Luego, se calcula la longitud de cada palabra y se suman todas las longitudes. Finalmente, se divide la suma total por el número total de palabras para obtener la longitud media.
+  //},
+
   getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+  TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+  const valor = text.value;
+  const regex = /\d+/g;
+  const matches = valor.match(regex);
+  const count = matches ? matches.length : 0;
+  return count;
   },
-  getNumberSum: (text) => {
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-  },
+
+  //getNumberSum: (text) => {
+  //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+  //},
 };
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
-import analyzer from './analyzer.js';
+import analyzer from "./analyzer.js";
+
+// Selectores del DOM
+const textarea = document.querySelector("textarea[name='user-input']");
+const wordcount = document.querySelector("li[data-testid='word-count']");
+const charactercount = document.querySelector(
+  "li[data-testid='character-count']"
+);
+const charactercountex = document.querySelector(
+  "li[data-testid='character-no-spaces-count']"
+);
+//const numbercount = document.querySelector("li[data-testid='number-count']");
+//const sumnumeros = document.querySelector("li[data-testid='numer-sum']");
+//const longitudmedia = document.querySelector("li[data-testid='word-lenght-average']");
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+textarea.addEventListener("keyup", () => {
+  //CONTADOR DE PALABRAS
+  const resultado = analyzer.getWordCount(textarea.value);
+  wordcount.innerHTML = "Palabras: " + resultado;
+  //CONTADOR DE CARACTERES
+  const resultadocaracteres = analyzer.getCharacterCount(textarea.value);
+  charactercount.innerHTML = "Caracteres: " + resultadocaracteres;
+  //CONTADOR DE CARACTERES SIN ESPACIOS
+  const resulcaracteressin = analyzer.getCharacterCountExcludingSpaces(
+    textarea.value
+  );
+  //console.log(resulcaracteressin);
+  charactercountex.innerHTML = "Caracteres sin espacios: " + resulcaracteressin;
+
+  //CONTADOR DE NÚMEROS
+  //const cantidadnumeros = analyzer.getNumberCount(textarea.value);
+  //console.log(cantidadnumeros);
+  //numbercount.innerHTML = "Números: " + cantidadnumeros;
+
+  //SUMA NÚMEROS
+  //const sumanumeros = analyzer.getNumberSum(textarea.value);
+  //console.log(sumanumeros);
+  //sumnumeros.innerHTML = "Suma números: " + sumanumeros;
+
+  //LONGITUD MEDIA
+  //const longitudmed = analyzer.getAverageWordLength(textarea.value);
+  //console.log(longitudmed);
+  //longitudmedia.innerHTML = "Longitud media: " + longitudmed;
+});
+
+//BOTÓN PARA LIMPIAR LOS DATOS
+const botonlimpiar = document.getElementById("reset-button");
+function limpiar() {
+  location.reload();
+}
+botonlimpiar.addEventListener("click", limpiar);
