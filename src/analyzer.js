0a0fbadab19624c83e30b4136405a344b822a53f
taciga30 <taciga30@gmail.com>
2023-09-05 15:25:56 -0500

    Final del proyecto

--- src/analyzer.js
+++ src/analyzer.js
@@ -19,11 +19,24 @@ const analyzer = {
   },
 
   getAverageWordLength: (text) => {
-  //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  //¿Cómo se calcula la longitud media de palabras en una cadena de texto?
-  //Para calcular la longitud media de palabras en una cadena de texto, primero hay que dividir el texto en palabras individuales. Luego, se calcula la longitud de cada palabra y se suman todas las longitudes. Finalmente, se divide la suma total por el número total de palabras para obtener la longitud media.
-  
-},
+    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    //¿Cómo se calcula la longitud media de palabras en una cadena de texto?
+    //Para calcular la longitud media de palabras en una cadena de texto, primero hay que dividir el texto en palabras individuales.
+    //Luego, se calcula la longitud de cada palabra y se suman todas las longitudes. Finalmente, se divide la suma total por el
+    //número total de palabras para obtener la longitud media.
+    const palabras = text.split(" ");
+    const numpalabras = palabras.filter((palabras) => palabras !== "").length;
+    let longitudes = 0;
+    if (numpalabras === 0) {
+      return 0;
+    } else {
+      for (let i = 0; i < palabras.length; i++) {
+        longitudes += palabras[i].length;
+      }
+      const promedio = longitudes / numpalabras;
+      return parseFloat(promedio.toFixed(2));
+    }
+  },
 
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
@@ -33,8 +46,7 @@ const analyzer = {
 
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-
-    const numeros = text.match(/\b\d+(\.\d+)?\b/g);
+    const numeros = text.match(/[-+]?\b\d+(\.\d+)?\b/g);
     if (!numeros) {
       return 0;
     } else {
@@ -43,7 +55,7 @@ const analyzer = {
           (total, number) => total + parseFloat(number, 10),
           0
         );
-        return suma;
+        return parseFloat(suma.toFixed(2));
       }
     }
   },
